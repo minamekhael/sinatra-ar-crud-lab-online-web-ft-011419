c664bf7569707285fda273a22e26ada16610fd86
Mina Mekhael <mina.mekhael@ymail.com>
2019-03-04 21:12:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +7,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+    get '/' do
+    redirect to "/articles"
+  end	
+
+   get '/articles/new' do
+  	erb :new
   end
-end
+
+   post '/articles' do
+  	Article.create(params)
+
+   	redirect "/articles/#{Article.last[:id]}"
+  end
+
+   get '/articles' do
+  	@articles = Article.all
+
+   	erb :index
+  end
+
+   get '/articles/:id' do
+  	@article = Article.find(params[:id])
+
+   	erb :show
+  end
+
+  get '/articles/:id/edit' do  #load edit form
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+   patch '/articles/:id' do
+  	@article = Article.find(params[:id])
+  	@article.title = params[:title]
+  	@article.content = params[:content]
+  	@article.save
+
+   	redirect to "/articles/#{@article.id}"
+  end
+
+   delete '/articles/:id' do
+  	@article = Article.find(params[:id])
+  	Article.delete(@article.id)
+
+   	@articles = Article.all
+  	erb :index
+  end
+
+ end	
